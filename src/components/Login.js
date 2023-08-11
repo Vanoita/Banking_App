a26e6c492ea9314f01e28eb68f237a0e799807c8
unknown <ak14502@gmail.com>
2023-08-11 13:25:34 +0530

    loginpage 2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { redirect } from 'react-router-dom';
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const baseURL = "http://localhost:9080/checkLogin";
+    const submitActionHandler = (event) => {
+        event.preventDefault();
+        // axios.post(baseURL, {
+        //     userId: username,
+        //     password: password
+        // }).then((response) => {
+        //     alert("User : "+ username + " Logged In.");
+        // }).catch(error => {
+        //     alert("error = " + error);
+        // });
+
+        alert(JSON.stringify({ 
+            username: username,
+            password: password
+        }));
+    };
     return (
         <div className="container d-flex flex-row justify-content-center align-items-center mt-4">
             <div className='card' style={{width:"100%", maxWidth:"500px"}}>
-            <form className="p-4">
+            <form className="p-4" onSubmit={submitActionHandler}>
                 <h3 className="my-4 text-center">Online Banking Application</h3>
                 <div class="form-outline mb-4">
-                    <input type="text" class="form-control" />
+                    <input type="text" class="form-control" value={username} onChange={e=>setUsername(e.target.value)}/>
                     <label class="form-label">Username</label>
                 </div>
                 <div class="form-outline mb-4">
-                    <input type="password" class="form-control" />
+                    <input type="password" class="form-control" value={password} onChange={e=>setPassword(e.target.value)} />
                     <label class="form-label">Password</label>
                 </div>
                 <div class="row mb-4">
