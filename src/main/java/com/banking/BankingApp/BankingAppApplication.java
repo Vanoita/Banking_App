994f15890c3d6801326f5d54de54e6266e1a0dbc
Abhinav Kumar <78070936+abhi5147@users.noreply.github.com>
2023-08-12 13:08:51 +0530

    Update BankingAppApplication.java

--- src/main/java/com/banking/BankingApp/BankingAppApplication.java
+++ src/main/java/com/banking/BankingApp/BankingAppApplication.java
@@ -1,5 +1,5 @@
 package com.banking.BankingApp;
-
+//main app
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
