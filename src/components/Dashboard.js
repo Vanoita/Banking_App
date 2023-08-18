06eb5c947ba58e29197a72d34a74937b53397c42
Vanoita <vanoita02@gmail.com>
2023-08-18 14:39:55 +0530

    dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,20 +2,72 @@ import { Helmet } from "react-helmet";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router";
 import { useState } from "react";
+import {
+    IconButton,
+    Avatar,
+    Box,
+    CloseButton,
+    Flex,
+    HStack,
+    VStack,
+    Icon,
+    useColorModeValue,
+    Text,
+    Drawer,
+    DrawerContent,
+    useDisclosure,
+    BoxProps,
+    FlexProps,
+    Menu,
+    MenuButton,
+    MenuDivider,
+    MenuItem,
+    MenuList,
+  } from '@chakra-ui/react'
+import axios from "axios";
 
 function Dashboard(){
     const [userId, setUserId] = useState("");
     const [alertMessage, setAlertMessage] = useState("");
     const [alertType, setAlertType] = useState("info");
+    const [accounts, setAccounts] = useState([]);
+    const [user, setUser] = useState({});
     const navigate = useNavigate();
     const {state} = useLocation();
     const location = useLocation();
+    const baseURLAccount = "http://localhost:8080/fetchAllAccount/"+userId;
+    const baseURLUser = "http://localhost:8080/fetchUser/DIT7D1U1"+userId;
+
+    const fetchAllAccount = () => {
+        axios
+            .get(baseURLAccount)
+            .then((response) => {
+                setAccounts(response.data);
+            })
+            .catch((error) => {
+                alert("error occured while loading data" + error);
+            });
+    };
+    const fetchUser = () => {
+        axios
+            .get(baseURLUser)
+            .then((response) => {
+                setUser(response.data);
+            })
+            .catch((error) => {
+                alert("error occured while loading data" + error);
+            });
+    };
     useEffect(() => {
         const token = localStorage.getItem('userId');
         if(!token) {
             navigate("/login",{state:{from: `${location.pathname}${location.search}`}});
         }
-        setUserId(token);
+        else {
+            setUserId(token);
+            fetchAllAccount();
+            fetchUser();
+        }
 
         if(state && state.message && state.type){
             setAlertMessage(state.message);
@@ -32,6 +84,7 @@ function Dashboard(){
             <div>
                 <h1>Welcome to Dashboard</h1>
                 <h2>{userId}</h2>
+
             </div>
         </>
     );
